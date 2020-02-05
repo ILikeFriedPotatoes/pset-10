ad8640355912da3d70b1332ed3160ac6a549e726
Joseph Wang <jwang@ucvts.org>
2020-02-05 11:56:03 -0500

    Created a framework for project

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -9,6 +9,7 @@ public class Dictionary extends JFrame {
 	static JPanel dictionaryWindow = new JPanel();
 	static JButton addButton = new JButton("Add");
 	static JButton removeButton = new JButton("Remove");
+	static JFrame frame = new JFrame("Joseph's Dictionary");
 	
 	//main function
 	public static void main(String[] args) {
@@ -18,25 +19,48 @@ public class Dictionary extends JFrame {
 	//creates the graphical application. Most code will go here.
 	
 	private static void dictionaryWindow() {
-		JFrame frame = new JFrame("Joseph's Dictionary");
 		frame.setSize(400, 300);
 		frame.setResizable(true);
 		
 		//creates the remove and add button
-		dictionaryWindow.add(addButton);
-		dictionaryWindow.add(removeButton);
-		frame.add(dictionaryWindow);
-		frame.setVisible(true);
+		buttons();
+		
+		//Views the words
+		mainViewWindow();
+		
+		search();
+		
+		sort();
+		
+		viewWords();
 		
 		//closes the dictionary
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
 	private static void buttons() {
+		dictionaryWindow.add(addButton);
+		dictionaryWindow.add(removeButton);
+		frame.add(dictionaryWindow);
+		frame.setVisible(true);
 		
 	}
 	
+	//shows the main viewing window
 	private static void mainViewWindow() {
+		
+	}
+	
+	// Shows the search bar of the function
+	private static void search() {
+		
+	}
+	
+	private static void sort() {
+		
+	}
+	
+	private static void viewWords() {
 		
 	}
 }
