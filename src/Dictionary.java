e99e3504f098f05a5dcdbd0aeb6d2565e632b71c
Joseph Wang <jwang@ucvts.org>
2020-02-05 09:11:08 -0500

    Added comments

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -4,16 +4,20 @@ import javax.swing.JPanel;
 
 import javax.swing.JButton;
 
+
+
 public class Dictionary extends JFrame {
 	
 	JPanel dictionaryWindow = new JPanel();
 	JButton removeButton = new JButton("Remove");
 	
+	//main function
 	public static void main(String[] args) {
-		new BasicSwing();
+		BasicSwing();
 		JFrame frame = new JFrame("Joseph's Dictionary");
 	}
 	
+	//creates the graphical application. Most code will go here.
 	private void BasicSwing() {
 		super("Dictionary App");
 		setSize(400, 300);
