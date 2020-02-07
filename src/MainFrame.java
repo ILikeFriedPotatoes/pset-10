b750212a7fb9f54b8d7fc59d0dd93e3d8b875104
Joseph Wang <jwang@ucvts.org>
2020-02-07 09:01:24 -0500

    Added actions to buttons

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -3,26 +3,53 @@ import javax.swing.JButton;
 import javax.swing.JTextArea;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame{
 	
-	private JTextArea textArea;
-	private JButton btn;
+	private JButton rmvBtn;
+	private JButton addBtn;
+	// I might use this for the search bar later:
+	//private JTextArea textArea;
 	
 	public MainFrame() {
 		super("Joseph's Dictionary");
 		
 		setLayout(new BorderLayout());
+			
+		showButtons();
 		
-		textArea = new JTextArea();
-		btn = new JButton();
-		
-		add(textArea, BorderLayout.CENTER);
-		add(btn, BorderLayout.SOUTH);
-		
-		setSize(400, 300);
+		setSize(600, 500);
 		setResizable(true);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
+	
+	
+	
+	private void showButtons() {
+		rmvBtn = new JButton("Remove");
+		addBtn = new JButton("Add");
+		
+		rmvBtn.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("does this work?");
+			}
+			
+		});
+		addBtn.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("does this work?");
+			}
+			
+		});
+		
+		add(rmvBtn, BorderLayout.NORTH);
+		add(addBtn, BorderLayout.NORTH);
+	}
+	
 }
+
