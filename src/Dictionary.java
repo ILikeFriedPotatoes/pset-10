f9c0068092f23cc8b3d53e49cd51fdf43c5df7ad
Joseph Wang <jwang@ucvts.org>
2020-02-07 08:44:07 -0500

    Deleted uncessary code

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,77 +1,17 @@
-import javax.swing.JFrame;
-import javax.swing.JMenuBar;
-import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
-import javax.swing.JButton;
 
-public class Dictionary extends JFrame {
-	
-	//Creates the graphical components of the app
-	static JPanel dictionaryWindow = new JPanel();
-	static JButton addButton = new JButton("Add");
-	static JButton removeButton = new JButton("Remove");
-	static JFrame frame = new JFrame("Joseph's Dictionary");
-	static JPanel viewWord = new JPanel();
-	static JMenuBar menuBar = new JMenuBar();
+public class Dictionary {
 	
 	//main function
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
+			
 			public void run() {
+				
 				new MainFrame();
-				dictionaryWindow();
+				
 			}	
 		});
-		dictionaryWindow();
 	}
 	
-	//creates the graphical application. Most code will go here.
-	
-	private static void dictionaryWindow() {
-		
-		
-		frame.setJMenuBar(menuBar);
-		
-		//creates the remove and add button
-		buttons();
-		
-		//Views the words
-		mainViewWindow();
-		
-		//Create the search bar to search for words
-		search();
-		
-		//Creates the ascending and descending checkboxes
-		sort();
-		
-		//Creates the list of words in the scrollbox
-		viewWords();
-		
-	}
-	
-	private static void buttons() {
-		dictionaryWindow.add(addButton);
-		dictionaryWindow.add(removeButton);
-		frame.add(dictionaryWindow);
-		frame.setVisible(true);
-		
-	}
-	
-	//shows the main viewing window
-	private static void mainViewWindow() {
-		frame.add(viewWord);
-	}
-	
-	// Shows the search bar of the function
-	private static void search() {
-		
-	}
-	
-	private static void sort() {
-		
-	}
-	
-	private static void viewWords() {
-		
-	}
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,11 +1,28 @@
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
 
 public class MainFrame extends JFrame{
+	
+	private JTextArea textArea;
+	private JButton btn;
+	
 	public MainFrame() {
 		super("Joseph's Dictionary");
 		
+		setLayout(new BorderLayout());
+		
+		textArea = new JTextArea();
+		btn = new JButton();
+		
+		add(textArea, BorderLayout.CENTER);
+		add(btn, BorderLayout.SOUTH);
+		
 		setSize(400, 300);
 		setResizable(true);
+		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 }
