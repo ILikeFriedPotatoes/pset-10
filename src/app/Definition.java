a08f0480970304ffdd3e3ad07a6fc1f9466626ea
Joseph Wang <jwang@ucvts.org>
2020-03-04 22:10:19 -0500

    Actually installed the Gson package

--- src/app/Definition.java
+++ src/app/Definition.java
@@ -1,9 +1,31 @@
 package app;
+import java.io.*;
+import java.util.*;
+import com.google.gson.*;
 
 public class Definition {
 	String definition;
     String POS;
-	    
+    private Words[] words;
+    
+	private void getGSON() {
+		Gson gson = new Gson();
+		try(Reader reader = new FileReader(System.getProperty("user.dir") + File.separator + "words.json")) {
+			this.words = gson.fromJson(reader, Words[].class);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void saveWords() {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (Writer writer = new FileWriter(System.getProperty("user.dir") + File.separator + "words.json")) {
+            gson.toJson(words, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
     public Definition(String definition, String POS) {
         this.definition = definition;
         this.POS = POS;
