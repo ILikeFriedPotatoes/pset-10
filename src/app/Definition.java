f459ad3bd25095f86fa566182e18653d2f267d17
Joseph Wang <jwang@ucvts.org>
2020-03-07 12:55:50 -0500

    Added words and definition class

--- src/app/Definition.java
+++ src/app/Definition.java
@@ -1,41 +1,19 @@
 package app;
-import java.io.*;
-import java.util.*;
-import com.google.gson.*;
 
 public class Definition {
-	String definition;
-    String POS;
-    private Words[] words;
+    private String definition;
+    private String partOfSpeech;
     
-	private void getGSON() {
-		Gson gson = new Gson();
-		try(Reader reader = new FileReader(System.getProperty("user.dir") + File.separator + "words.json")) {
-			this.words = gson.fromJson(reader, Words[].class);
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void saveWords() {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        try (Writer writer = new FileWriter(System.getProperty("user.dir") + File.separator + "words.json")) {
-            gson.toJson(words, writer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public Definition(String definition, String POS) {
+    public Definition(String definition, String partOfSpeech) {
         this.definition = definition;
-        this.POS = POS;
+        this.partOfSpeech = partOfSpeech;
     }
-	    
+    
     public String getDefinition() {
-    	return definition;
+        return definition;
     }
-	    
-	public String getPOS() {
-		return POS;
-	}
+    
+    public String getPart() {
+        return partOfSpeech;
+    }
 }
--- src/app/Words.java
+++ src/app/Words.java
@@ -3,32 +3,54 @@ import java.io.FileReader;
 import java.util.ArrayList;
 
 public class Words {
-	private String word;
-	private ArrayList<String> Definitions;
-	private ArrayList<String> synonyms;
-	private ArrayList<String> antonyms;
-	
-	public Words(String word, ArrayList<String> Definition, ArrayList<String> synonyms, ArrayList<String> antonyms) {
-		this.word = word;
-		this.Definitions = Definition;
-		this.synonyms = synonyms;
-		this.antonyms = antonyms;
-	}
-	
-	public String getWord() {
+    private String word;
+    private Definition[] definitions;
+    private String[] synonyms;
+    private String[] antonyms;
+    
+    public Words(String word, Definition[] definitions, String[] synonyms, String[] antonyms) {
+        this.word = word;
+        this.definitions = definitions;
+        this.synonyms = synonyms;
+        this.antonyms = antonyms;
+    }
+    
+    public Words(String word, String definition, String partOfSpeech, String[] synonyms, String[] antonyms) {
+        this.word = word;
+        this.definitions = new Definition[1];
+        definitions[0] = new Definition(definition, partOfSpeech);
+        this.synonyms = synonyms;
+        this.antonyms = antonyms;
+    }
+    
+    public Words(String word, String[] definition, String[] partOfSpeech, String[] synonyms, String[] antonyms) {
+        this.word = word;
+        this.definitions = popDef(definition, partOfSpeech);
+        this.synonyms = synonyms;
+        this.antonyms = antonyms;
+    }
+    
+    private Definition[] popDef(String[] definition, String[] partOfSpeech) {
+        Definition[] defs = new Definition[definition.length];
+        for (int i = 0; i < definition.length; i++) {
+            defs[i] = new Definition(definition[i], partOfSpeech[i]);
+        }
+        return defs;
+    }
+    
+    public String getWord() {
         return word;
     }
     
-    public ArrayList<String> getSyn() {
+    public String[] getSyn() {
         return synonyms;
     }
     
-    public ArrayList<String> getAnt() {
+    public String[] getAnt() {
         return antonyms;
     }
     
-    public ArrayList<String> getDefinitions() {
-    	return Definitions;
+    public Definition[] getDefinitions() {
+        return definitions;
     }
-    
 }
